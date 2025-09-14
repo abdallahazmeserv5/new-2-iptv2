151f01c0dac19fd45d333d57a936af4e3a765596
abdallah-azme <abdallah@serv5.info>
2025-09-14 11:47:10 +0300

    env

--- src/modules/shared/components/image-fall-back.tsx
+++ src/modules/shared/components/image-fall-back.tsx
@@ -4,16 +4,5 @@ interface ImageFallBackProps extends React.ImgHTMLAttributes<HTMLImageElement> {
 }
 
 export default function ImageFallBack({ src, alt, fallbackSrc, ...rest }: ImageFallBackProps) {
-  // Use fallback if src is missing
-  let resolvedSrc = src || fallbackSrc || '/default.png'
-
-  // Remove /api prefix for Payload images
-  resolvedSrc = resolvedSrc.replace(/^\/api/, '')
-
-  // Prepend absolute URL in production
-  if (resolvedSrc.startsWith('/')) {
-    resolvedSrc = `${process.env.NEXT_PUBLIC_APP_URL || ''}${resolvedSrc}`
-  }
-
-  return <img src={resolvedSrc} alt={alt || ''} {...rest} />
+  return <img src={src} alt={alt || ''} {...rest} />
 }
